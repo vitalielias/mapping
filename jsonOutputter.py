962d7387a5cf0767331b278ee7f77f49727b86dd
Elias Vitali <elias.vitali@kit.edu>
2023-09-17 15:55:38 +0200

    zipping of output files

--- jsonOutputter.py
+++ jsonOutputter.py
@@ -1,4 +1,6 @@
 import json
+import os
+import zipfile
 
 class JsonOutputter:
     def __init__(self, mapped_metadata):
@@ -24,3 +26,28 @@ class JsonOutputter:
         nested_json = self.generate_nested_json()
         with open(file_path, 'w') as f:
             json.dump(nested_json, f, indent=4)
+        return file_path  # Return the path of the saved file
+    
+    @staticmethod
+    def save_to_zip(zip_filename, data_list, outputPath):
+        with zipfile.ZipFile(zip_filename, 'w') as zipf:
+            for idx, data in enumerate(data_list):
+                outputter = JsonOutputter(data)
+                nested_json = outputter.generate_nested_json()
+                json_content = json.dumps(nested_json, indent=4)
+                output_file_name = f"{os.path.basename(zip_filename).replace('.zip', '')}_{idx}.json"
+                zipf.writestr(output_file_name, json_content)
+
+        JsonOutputter.remove_hidden_files(outputPath)
+
+    @staticmethod
+    def remove_hidden_files(directory):
+        for filename in os.listdir(directory):
+            if filename.startswith('.'):
+                try:
+                    os.remove(os.path.join(directory, filename))
+                except Exception as e:
+                    print(f"Error deleting hidden file {filename}: {e}")
+
+
+
--- testScript.py
+++ testScript.py
@@ -1,7 +1,6 @@
 from semReader import semReader
 from semMapper import semMapper
 from jsonOutputter import JsonOutputter
-import json
 import os
 import shutil
 
@@ -13,20 +12,30 @@ def process_file(file_path, mapFile, outputPath):
     mapper = semMapper(metadata_list, mapFile)
     mapped_metadata_list = mapper.get_mapped_metadata()
 
+    generated_files = []  # List to store paths of generated JSON files
+
     for idx, mapped_metadata in enumerate(mapped_metadata_list):
         outputter = JsonOutputter(mapped_metadata)
         output_file_name = f"{fileName}_{idx}.json" if len(mapped_metadata_list) > 1 else f"{fileName}.json"
-        outputter.save_to_file(os.path.join(outputPath, output_file_name))
-    return reader
+        saved_file_path = outputter.save_to_file(os.path.join(outputPath, output_file_name))
+        generated_files.append(saved_file_path)
+
+    return reader, generated_files, mapped_metadata_list
 
 if __name__ == "__main__":
-    # input_file_path = '/Users/elias/Desktop/MatWerk_Projects/SEMtestImages/csv_test_images.zip'
-    input_file_path = '/Users/elias/Desktop/MatWerk_Projects/SEMtestImages/Au-Gr_06.tif'
+    input_file_path = '/Users/elias/Desktop/MatWerk_Projects/SEMtestImages/csv_test_images.zip'
+    # input_file_path = '/Users/elias/Desktop/MatWerk_Projects/SEMtestImages/Au-Gr_06.tif'
     mapFile = '/Users/elias/Desktop/MatWerk_Projects/mapping/map_files/hsSemMap.json'
     outputPath = '/Users/elias/Desktop/MatWerk_Projects/mapping/results'
     
-    reader_instance = process_file(input_file_path, mapFile, outputPath)
+    reader_instance, generated_files, mapped_metadata_list = process_file(input_file_path, mapFile, outputPath)
     
+    # If the input was a zip file and multiple JSON files were generated, zip them together
+    if input_file_path.endswith('.zip') and len(generated_files) > 1:
+        zip_filename = os.path.join(outputPath, os.path.basename(input_file_path).replace('.zip', '_metadata_documents.zip'))
+        JsonOutputter.save_to_zip(zip_filename, mapped_metadata_list, outputPath)
+    
+    # Clean up the temporary directory used for unzipping
     temp_dir = reader_instance.get_temp_dir_path()
     if os.path.exists(temp_dir):
         try:
